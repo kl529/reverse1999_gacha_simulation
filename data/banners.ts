d7fbd1861438a11b8aed0e77ab3baba42ab7d81d
kl529 <kl529@naver.com>
2025-02-24 21:30:01 +0900

    add flutter page banner

--- data/banners.ts
+++ data/banners.ts
@@ -10,14 +10,22 @@ export type Banner = {
 // 예제 배너 데이터 (추가 가능)
 export const banners: Banner[] = [
   {
-    id: "lopera_pick_up",
-    name: "로페라 픽업",
-    pickup6: { name: "로페라", rarity: 6, inspiration: "beast", engName: "lopera" },
+    id: "flutter_page_pick_up",
+    name: "플러터 페이지 픽업",
+    pickup6: { name: "플러터 페이지", rarity: 6, inspiration: "star", engName: "flutter-page" },
     pickup5: [
-      { name: "클릭", rarity: 5, inspiration: "spirit", engName: "click" },
-      { name: "울루", rarity: 5, inspiration: "mineral", engName: "ulu" },
+      { name: "슬라우치 햇", rarity: 5, inspiration: "mineral", engName: "brimley" }
     ],
   },
+  {
+    id: "isolde_pick_up",
+    name: "이졸데 픽업",
+    pickup6: { name: "이졸데", rarity: 6, inspiration: "spirit", engName: "isolde" },
+    pickup5: [
+      { name: "찰리", rarity: 5, inspiration: "star", engName: "charlie" },
+      { name: "클릭", rarity: 5, inspiration: "spirit", engName: "click" },
+    ],
+  }, 
   {
     id: "willow_pick_up",
     name: "윌로우 픽업",
@@ -28,12 +36,12 @@ export const banners: Banner[] = [
     ],
   },
   {
-    id: "isolde_pick_up",
-    name: "이졸데 픽업",
-    pickup6: { name: "이졸데", rarity: 6, inspiration: "spirit", engName: "isolde" },
+    id: "lopera_pick_up",
+    name: "로페라 픽업",
+    pickup6: { name: "로페라", rarity: 6, inspiration: "beast", engName: "lopera" },
     pickup5: [
-      { name: "찰리", rarity: 5, inspiration: "star", engName: "charlie" },
       { name: "클릭", rarity: 5, inspiration: "spirit", engName: "click" },
+      { name: "울루", rarity: 5, inspiration: "mineral", engName: "ulu" },
     ],
   },
 ];
