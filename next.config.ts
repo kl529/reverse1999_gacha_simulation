ce43744549adacd34ee82d57a8635ab9521e4f98
kl529 <kl529@naver.com>
2025-03-12 22:00:06 +0900

    set cloudflare build setting

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  output: "export"
 };
 
 export default nextConfig;
