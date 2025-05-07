7b224ec47911104afaf16484a0bea47a8c04fd59
kl529 <kl529@naver.com>
2025-05-07 21:34:11 +0900

    페이지 이름 수정

--- app/character_setting/[id]/page.tsx
+++ app/character_setting/[id]/page.tsx
@@ -15,7 +15,7 @@ export async function generateMetadata({ params }: { params: Promise<{ id: strin
   if (!character) return {};
 
   const baseUrl = "https://www.reverse1999-simulator.com";
-  const title = `Reverse: 1999 ${character.name} 공명 & 의지 세팅`;
+  const title = `리버스 1999 ${character.name} 공명 & 의지 세팅`;
   const description = `${character.name}의 공명과 의지 추천 세팅을 확인해보세요.`;
   const imageUrl = `${baseUrl}/characters/${character.rarity}stars_small/${character.engName}.png`;
 
--- app/character_setting/page.tsx
+++ app/character_setting/page.tsx
@@ -10,15 +10,15 @@ export default function CharacterSettingPage() {
 
 export const metadata = {
   metadataBase: new URL("https://www.reverse1999-simulator.com"),
-  title: "Reverse:1999 공명 & 의지 세팅",
-  description: "Reverse:1999 모든 캐릭터의 공명 & 의지 세팅을 찾아보세요!",
+  title: "리버스 1999 공명 & 의지 세팅",
+  description: "리버스 1999 모든 캐릭터의 공명 & 의지 세팅을 찾아보세요!",
   icons: {
     icon: "/pwa_icon.png", // PWA 아이콘
   },
   manifest: "/manifest.json", // PWA manifest 파일
   openGraph: {
-    title: "Reverse:1999 공명 & 의지 세팅",
-    description: "Reverse:1999 모든 캐릭터의 공명 & 의지 세팅을 찾아보세요!",
+    title: "리버스 1999 공명 & 의지 세팅",
+    description: "리버스 1999 모든 캐릭터의 공명 & 의지 세팅을 찾아보세요!",
     url: "https://www.reverse1999-simulator.com/character_setting",
     siteName: "버틴의 여행가방",
     images: [
@@ -26,15 +26,15 @@ export const metadata = {
         url: `/infos/link_img/character_setting_link_img.png`,
         width: 1200,
         height: 630,
-        alt: "Reverse:1999 공명 & 의지 세팅 미리보기 이미지",
+        alt: "리버스 1999 공명 & 의지 세팅 미리보기 이미지",
       },
     ],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Reverse:1999 공명 & 의지 세팅",
-    description: "Reverse:1999 모든 캐릭터의 공명 & 의지 세팅을 찾아보세요!",
+    title: "리버스 1999 공명 & 의지 세팅",
+    description: "리버스 1999 모든 캐릭터의 공명 & 의지 세팅을 찾아보세요!",
     images: [`/infos/link_img/character_setting_link_img.png`],
   },
 };
