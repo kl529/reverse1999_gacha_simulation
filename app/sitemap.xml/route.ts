91d665c5ca582e5442b12780aa9775284be21945
kl529 <kl529@naver.com>
2025-05-07 21:29:03 +0900

    사이트맵 오타 수정

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -3,7 +3,7 @@ import { SETTING_CHARACTERS } from "@/data/setting_character";
 
 export async function GET() {
   const baseUrl = "https://www.reverse1999-simulator.com";
-  const staticUrls = ["/", "gacha_simulator", "/character_quiz", "/character_setting", "/skin", "/path_quiz"];
+  const staticUrls = ["/", "/gacha_simulator", "/character_quiz", "/character_setting", "/skin", "/path_quiz"];
 
   const characterUrls = SETTING_CHARACTERS.map(
     (c) => `${baseUrl}/character_setting/${c.id}`
